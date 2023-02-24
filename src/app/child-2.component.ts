aa2a7a40dbcc3795cc7ad81f5cc63c2d7d107212
Isaac Lyman <isaac.lyman@healthcatalyst.com>
2023-02-24 12:09:49 -0700

    Add button to child 2

--- src/app/child-2.component.ts
+++ src/app/child-2.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'child-2',
+  selector: "child-2",
   template: `
     <div>Child 2</div>
+    <div>
+      <button (click)="addQueryParams()">Add Query Params</button>
+    </div>
   `,
 })
-export class Child2Component {}
+export class Child2Component {
+  constructor(private router: Router) {}
+
+  addQueryParams(): void {
+    this.router.navigate([], {
+      queryParams: {
+        param1: "alice",
+        param2: "bob",
+      },
+      queryParamsHandling: "merge",
+    });
+  }
+}
